ca7ddc601c85cf738f67de85bdc1ee155f96cb5b
Nithish Velu <n.kumaraguru540@mybvc.ca>
2025-12-03 18:29:36 -0700

    Adding Inventory
    Moved Scripts

--- Assets/Tutorial_Scripts/MVC/Items/Health_PU.cs
+++ Assets/Tutorial_Scripts/MVC/Items/Health_PU.cs
@@ -9,17 +9,10 @@ public class Health_PU : MonoBehaviour, ILifeCollectable
     public int HealthAmount => healthAmount;
 
     public UnityEvent OnCollected;
-    //public UnityEvent<int> OnHealthRestored;
+
 
     public void Collect(GameObject collector)
     {
-        //PlayerHealth playerHealth = collector.GetComponent<PlayerHealth>();
-        //if (playerHealth != null)
-        //{
-        //    playerHealth.RestoreHealth(healthAmount);
-        //    OnHealthRestored?.Invoke(healthAmount);
-        //}
-
         OnCollected?.Invoke();
         Destroy(gameObject);
     }
@@ -28,7 +21,7 @@ public class Health_PU : MonoBehaviour, ILifeCollectable
     {
         if (other.TryGetComponent<PlayerHealth>(out var playerHealth))
         {
-            //Collect(other.gameObject);
+            
             InventoryController.Instance.AddItemToInventory(_itemData);
             Destroy(gameObject);
         }
